0c43201ff2951aa95d2cb17729e3c66a78190ad2
chetan520 <chetanrox520@gmail.com>
2018-12-14 12:00:42 +0530

    update

--- Song.py
+++ Song.py
@@ -1,29 +1,41 @@
+#Script to Download Songs all songs (By artist or By Movie name)
+#This script only works on WWW.pagalworal.io
+
+#Modules to sent request to server
 import requests
+#Module to handel the content of site 
 from bs4 import BeautifulSoup
 import os
 
-#specify the url
+#Input [artist name or movie name]
 Artist_name =input("Artist Name or Movie Name :").replace(' ','+')
-stype ="5570"
+
+#Domain form are we are downloading song
 domain_name ="https://www.pagalworld.io"
-wiki = "https://www.pagalworld.io/search?cats="+stype+"&q="+Artist_name+"&page="
+URL = "https://www.pagalworld.io/search?cats=5570&q="+Artist_name+"&page="
 page = 1
 
-Link_list =[]
-song_name =[]
+song_links =[]	#Links of songs
+song_names =[]	#Song names
 prv =[]
 while True:
     cur =[]
-    source = requests.get(wiki+str(page)).text
+    #sending request to server and storing site content to source
+    source = requests.get(URL+str(page)).text
     page+=1
+    #Beautifulsoup is useto handle site content [HTML,CSS,etc souce code of site]
     soup = BeautifulSoup(source,"lxml")
-    link_data =soup.find_all('div', class_='listbox')
-    if len(link_data)>0:
-        for href in link_data:
+
+    #Fnding all division tag [because all the links of songes are in Div tag]
+    div_tag_data =soup.find_all('div', class_='listbox')
+
+    #Finding all the link and song name from all div tag and storing in list
+    if len(div_tag_data)>0:
+        for href in div_tag_data:
             cell = href.find('a')
             if cell.text.strip() not in song_name:
-                Link_list.append(cell['href'])
-                song_name.append(cell.text.strip())
+                song_links.append(cell['href'])
+                song_names.append(cell.text.strip())
                 cur.append(cell.text.strip())
     else:
         print("No record found !!!")
@@ -33,22 +45,28 @@ while True:
 
 print(Artist_name,"Songs :-")
 count=0
-for song in song_name:
+
+#Display all song according to search result
+for song in song_names:
     print("\t\t",count,song)
     count+=1
+
+#Input to select song to download
 song_num = list(map(int,input("Enter song Numbers(sepersted by ','):").split(",")))
+
+#Code to download song
 n=0    
 for i in song_num:
     os.system('clear')
     n+=1
-    response=requests.get(domain_name+Link_list[i]).text
+    response=requests.get(domain_name+song_links[i]).text
     soup1 = BeautifulSoup(response,"lxml")
     result= soup1.find('a',class_='dbutton')
     downlink= result['href']
     songname=result['title']
     print("[ Downloding ",n,"-",len(song_num),"]")
-    print(result['title'])
-    drspn = requests.get(downlink)
+    print(songname[9:])
+    download_song = requests.get(downlink)
     file = open(songname[9:],'wb')
-    file.write(drspn.content)
+    file.write(download_song.content)
     
